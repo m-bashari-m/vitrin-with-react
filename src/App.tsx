bf9be1787364b41659f358822f32d5ae6b5689f0
Mojtaba Bashari <m-bashari@outlook.com>
2023-07-26 08:43:03 +0330

    Modify: File structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import Footer from "./components/footer/Footer";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
