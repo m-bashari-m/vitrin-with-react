227d11d2a40fcd4f40cbcb03fa936e1d44dc5491
Mojtaba Bashari <m-bashari@outlook.com>
2023-07-28 01:02:30 +0330

    Add: BannerSlider

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,4 +1,5 @@
 import { offers, categories } from "../../data/data";
+import BannerSlider from "./BannerSlider";
 import SliderCard from "./SliderCard";
 import SliderSection from "./SliderSection";
 
@@ -6,16 +7,20 @@ const Main: React.FC = () => {
   return (
     <div className="flex justify-center w-full">
       <main className="w-full my-4 mt-32 sm:mt-[130px] md:w-[552px] lg:w-[800px] bg-white flex justify-center flex-col items-center rounded-3xl overflow-visible md:overflow-hidden">
-        <SliderSection showAllHref="#" sliderTitle="مذهبی">
-          {offers[0].data.map((data) => (
-            <SliderCard
-              key={data.id}
-              cardSrc={data.image}
-              title={data.title}
-              alt={data.alt}
-            />
-          ))}
-        </SliderSection>
+        <BannerSlider />
+
+        {offers.map((offer) => (
+          <SliderSection showAllHref="#" sliderTitle={offer.id} key={offer.id}>
+            {offer.data.map((data) => (
+              <SliderCard
+                key={data.id}
+                cardSrc={data.image}
+                title={data.title}
+                alt={data.alt}
+              />
+            ))}
+          </SliderSection>
+        ))}
       </main>
     </div>
   );
--- src/components/main/SliderCard.tsx
+++ src/components/main/SliderCard.tsx
@@ -6,18 +6,20 @@ interface SliderCardProps {
 
 const SliderCard: React.FC<SliderCardProps> = (props) => {
   return (
-    <a className="flex flex-col justify-center items-end pr-2 flex-grow-0 flex-shrink-0 flex-auto">
-      <img
-        src={props.cardSrc}
-        alt={props.alt}
-        loading="lazy"
-        draggable="false"
-        className="rounded-3xl w-[75px] h-[75px] sm:w-[90px] sm:h-[90px] mb-2 object-cover"
-      />
-      <p className="flex flex-wrap gap-2 justify-center items-start">
-        {props.title}
-      </p>
-    </a>
+    <div className="w-full flex justify-end">
+      <a className="flex flex-col justify-center mr-2 self-start flex-grow-0 flex-shrink-0 flex-auto hover:cursor-pointer w-fit">
+        <img
+          src={props.cardSrc}
+          alt={props.alt}
+          loading="lazy"
+          draggable="false"
+          className="rounded-3xl w-[75px] h-[75px] sm:w-[90px] sm:h-[90px] mb-2 object-cover"
+        />
+        <p className="flex flex-wrap justify-center items-start">
+          {props.title}
+        </p>
+      </a>
+    </div>
   );
 };
 
